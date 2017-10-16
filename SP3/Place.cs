f60a4e115a99058dfc82ad3e0a0daefb7fb19b4b
lmb <1603497362@qq.com>
2017-10-16 17:53:01 +0800

    commit

--- SP3/Place.cs
+++ SP3/Place.cs
@@ -5,25 +5,44 @@ using System.Net.Http;
 using System.IO;
 using System.Threading.Tasks;
 using System;
+using CsQuery;
 
 namespace SP3
 {
+    public enum PlaceType
+    {
+        Nation, Province, City, County, Town, Village
+    }
+
     class Place
     {
         public string Name { get; set; }
         public string URL { get; set; }
         public string Code { get; set; }
         public string Type { get; set; }
-
-        public List<Place> Children { get; set; }
+        public PlaceType PlaceType { get; set; }
         public Place Father { get; set; }
+        public List<Place> Children { get; set; }
+
 
         public bool Traversed { get; set; }
 
-
+        /// <summary>
+        /// 构造函数
+        /// </summary>
         public Place()
         {
+            Traversed = false;
 
+        }
+
+
+        public async Task ClickInAsync()
+        {
+            WebClient client = new WebClient();
+
+            string a = await client.DownloadStringTaskAsync(URL);
+            Console.WriteLine(a);
         }
 
         public void Start()
@@ -43,6 +62,7 @@ namespace SP3
             WebClient client = new WebClient();
             client.DownloadStringAsync(new Uri(URL));
             client.DownloadStringCompleted += OnDownloaded;
+
         }
 
         private void OnDownloaded(object sender, DownloadStringCompletedEventArgs e)
@@ -55,7 +75,7 @@ namespace SP3
             {
                 OnDownloadSuccess(e.Result);
             }
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
         }
         private void OnDownloadSuccess(string result)
         {
@@ -63,16 +83,37 @@ namespace SP3
 
         }
 
-        private List<Place> TryGetChildren(string result)
+        public virtual List<Place> TryGetChildren(string result)
         {
-
-
-
             return null;
-
         }
 
+        //private List<Place> TryGetChildren(string result)
+        //{
+        //    //List<Place> childrenCollection = new List<Place>();
 
+        //    //switch (PlaceType)
+        //    //{
+        //    //    default: break;
+        //    //        //case PlaceType.Nation:
+        //    //        //    {
+        //    //        //        childrenCollection = TryGetNationChildren(result);
+        //    //        //        break;
+        //    //        //    };
+        //    //        //case PlaceType.Province:
+        //    //        //    {
+        //    //        //        childrenCollection = TryGetProvinceChildren(result);
+        //    //        //        break;
+        //    //        //    }
+        //    //        //case PlaceType.City:
+        //    //        //    {
+        //    //        //        childrenCollection = TryGetCityChildren(result);
+        //    //        //        break;
+        //    //        //    };
+        //    //}
+        //    //return childrenCollection;
+        //    ////return null;
+        //}
 
         private void OnGetPage()
         {
@@ -85,8 +126,176 @@ namespace SP3
         public string FullName(string split) => Father != null ? Father.FullName() + split + Name : Name;
         public string FullName(char split) => Father != null ? Father.FullName() + split + Name : Name;
 
+        //public virtual List<Place> TryGetChildren(string htmlResult) { }
+        delegate List<Place> TryGetChildrenCollection(string htmlResult);
+
+        public virtual List<Place> TryGetNationChildren(string htmlResult)
+        {
+            //CQ cq = new CQ(htmlResult);
+            CQ elements = new CQ(htmlResult)[".provincetr a"];
+            List<Place> provinces = new List<Place>();
+            foreach (var element in elements)
+            {
+                Place province = new Place
+                {
+                    PlaceType = PlaceType.Province,
+                    Name = element.FirstChild.ToString()
+                };
+                if (element.HasAttribute("href"))
+                {
+                    province.URL = this.URL + element.Attributes.GetAttribute("href");
+                }
+                province.Father = this;
+                provinces.Add(province);
+            }
+            return provinces;
+            //return null;
+        }
+        public virtual List<Place> TryGetProvinceChildren(string htmlResult)
+        {
+            CQ elements = new CQ(htmlResult)[".citytr"];
+            List<Place> cities = new List<Place>();
+            foreach (var element in elements)
+            {
+                Place city = new Place()
+                {
+                    PlaceType = PlaceType.City,
+                    Name = element.ChildNodes[1].FirstChild.FirstChild.ToString()
+                };
+                city.Father = this;
+                if (element.FirstChild.FirstChild.HasAttribute("href"))
+                {
+                    //var stringArray = element.FirstChild.FirstChild.Attributes.GetAttribute("href").Split('/');
+                    var stringArray = URL.Split('/');
+                    stringArray[stringArray.Length - 1] = element.FirstChild.FirstChild.Attributes.GetAttribute("href");
+                    var newstring = String.Join("/", stringArray);
+                    city.URL = newstring;
+                    //city.Father.Father
+                }
+                cities.Add(city);
+            }
+            return cities;
+        }
+        public virtual List<Place> TryGetCityChildren(string htmlResult)
+        {
+            CQ elements = new CQ(htmlResult)[".countytr"];
+            List<Place> counties = new List<Place>();
+            foreach (var element in elements)
+            {
+                Place county = new Place();
+                county.PlaceType = PlaceType.County;
+                if (element.FirstChild.FirstChild.HasAttribute("href"))
+                {
+                    var stringArray = URL.Split('/');
+                    stringArray[stringArray.Length - 1] = element.FirstChild.FirstChild.Attributes.GetAttribute("href");
+                    var newString = String.Join("/", stringArray);
+                    county.URL = newString;
+                    county.Name = element.ChildNodes[1].FirstChild.FirstChild.ToString();
+                }
+                else
+                {
+                    county.Name = element.ChildNodes[1].FirstChild.ToString();
+                    county.Traversed = true;
+                }
+                county.Father = this;
+                counties.Add(county);
+            }
+            return counties;
+        }
+
+        //public List<Place> TryGetCountyChildren(string htmlResult)
+        //{
+        //    CQ elements = new CQ(htmlResult)[".towntr"];
+        //    List<Place> towns = new List<Place>();
+
+        //}
+
 
 
     }
+    class Nation : Place
+    {
+        public override List<Place> TryGetChildren(string htmlResult)
+        {
+            //CQ cq = new CQ(htmlResult);
+            CQ elements = new CQ(htmlResult)[".provincetr a"];
+            List<Place> provinces = new List<Place>();
+            foreach (var element in elements)
+            {
+                Province province = new Province
+                {
+                    PlaceType = PlaceType.Province,
+                    Name = element.FirstChild.ToString()
+                };
+                if (element.HasAttribute("href"))
+                {
+                    province.URL = this.URL + element.Attributes.GetAttribute("href");
+                }
+                province.Father = this;
+                provinces.Add(province);
+            }
+            return provinces;
+        }
+    }
+
+    class Province : Place
+    {
+        public override List<Place> TryGetChildren(string htmlResult)
+        {
+            CQ elements = new CQ(htmlResult)[".citytr"];
+            List<Place> cities = new List<Place>();
+            foreach (var element in elements)
+            {
+                City city = new City()
+                {
+                    PlaceType = PlaceType.City,
+                    Name = element.ChildNodes[1].FirstChild.FirstChild.ToString()
+                };
+                city.Father = this;
+                if (element.FirstChild.FirstChild.HasAttribute("href"))
+                {
+                    var stringArray = URL.Split('/');
+                    stringArray[stringArray.Length - 1] = element.FirstChild.FirstChild.Attributes.GetAttribute("href");
+                    var newstring = String.Join("/", stringArray);
+                    city.URL = newstring;
+                }
+                cities.Add(city);
+            }
+            return cities;
+        }
+    }
+
+    class City : Place
+    {
+        public override List<Place> TryGetChildren(string htmlResult)
+        {
+            CQ elements = new CQ(htmlResult)[".countytr"];
+            List<Place> counties = new List<Place>();
+            foreach (var element in elements)
+            {
+                Place county = new Place();
+                county.PlaceType = PlaceType.County;
+                if (element.FirstChild.FirstChild.HasAttribute("href"))
+                {
+                    var stringArray = URL.Split('/');
+                    stringArray[stringArray.Length - 1] = element.FirstChild.FirstChild.Attributes.GetAttribute("href");
+                    var newString = String.Join("/", stringArray);
+                    county.URL = newString;
+                    county.Name = element.ChildNodes[1].FirstChild.FirstChild.ToString();
+                }
+                else
+                {
+                    county.Name = element.ChildNodes[1].FirstChild.ToString();
+                    county.Traversed = true;
+                }
+                county.Father = this;
+                counties.Add(county);
+            }
+            return counties;
+        }
+    }
+
+
+
 
 }
--- SP3/Program.cs
+++ SP3/Program.cs
@@ -4,6 +4,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Collections.Generic;
 using System.Collections;
+using System.Threading;
 
 namespace SP3
 {
@@ -13,22 +14,45 @@ namespace SP3
         {
 
 
-
-            Place china = new Place
+            Nation china = new Nation
             {
                 Name = "中华人民共和国",
                 URL = "http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/2016/",
-                Type = "Nation",
+                //Type = "Nation",
+                PlaceType = PlaceType.Nation,
+                Traversed = false,
             };
+            //Task task = china.ClickInAsync();
+            //Console.WriteLine(task.AsyncState);
             china.Start();
+            Province hebei = new Province
+            {
+                Name = "河北省",
+                URL = "http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/2016/13.html",
+                //Type = "Nation",
+                PlaceType = PlaceType.Province,
+                Traversed = false,
+            };
+            hebei.Start();
+            City shijiazhuang = new City
+            {
+                Name = "石家庄市",
+                URL = "http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/2016/13/1301.html",
+                //Type = "Nation",
+                PlaceType = PlaceType.City,
+                Traversed = false,
+            };
+            shijiazhuang.Start();
+
+            Thread.Sleep(2000);
             Console.ReadLine();
         }
     }
 
-    class DownloadQuene
+    static class DownloadQuene
     {
-        public Queue<Place> PlacesToClick { get; set; }
-        public void Start()
+        static public Queue<Place> PlacesToClick { get; set; }
+        static public void Start()
         {
 
         }
